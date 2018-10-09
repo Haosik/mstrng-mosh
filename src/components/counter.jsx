3bd296daf7e8e0630af028f5cc21aca57eabd11c
Haosik <noobbaster@gmail.com>
2018-10-09 20:14:18 +0300

    les 26

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = { count: 0 };
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  render() {
+    return (
+      <>
+        <span>{this.formatCount()}</span>
+        <button onClick={this.increment}>Increment</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
