890d3cd05c18265627ab29caabf795c7a00f3ffb
Haosik <noobbaster@gmail.com>
2018-10-12 02:06:28 +0300

    counter again

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
-import App from './App';
+
+// import App from './App';
+import Counters from './components/counters';
 
 // import Counter from './components/counter';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counters />, document.getElementById('root'));
